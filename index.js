49ac011bfa81fd099eb5c442c9a7b1a077dea4b1
Theo Winter <theo.winter@gmail.com>
2017-02-21 21:14:02 +0100

    Added button page for local testing.

--- index.js
+++ index.js
@@ -16,17 +16,17 @@ var urlencodedParser = bodyParser.urlencoded({extended: false});
  */
 var isNight = function () {
     var currentD = new Date();
-    var startNight = new Date().setHours(12, 30, 0);
-    var endNight = new Date().setHours(18, 30, 0);
+    var startNight = new Date().setHours(20, 30, 0);
+    var endNight = new Date().setHours(8, 30, 0);
     return !(currentD >= startNight && currentD < endNight);
 };
 
 app.post('/google-home-notifier', urlencodedParser, function (req, res) {
     if (!req.body) return res.sendStatus(400);
-    console.log(req.body);
     var text = req.body.text;
     if (isNight()) {
         if (text) {
+            console.log('Sending "' + text + '" to Google Home!');
             res.send(deviceName + ' will say: ' + text + '\n');
             googlehome.notify(text, function (res) {
                 console.log(res);
@@ -35,15 +35,18 @@ app.post('/google-home-notifier', urlencodedParser, function (req, res) {
             res.send('Please POST "text=Hello Google Home"');
         }
     } else {
-        var message = "Suppressed notification because it is night!";
+        var message = 'Suppressed notification: ' + text + ' because it is night!';
         res.send(message);
         console.log(message);
     }
 });
 
 app.listen(serverPort, function () {
+    console.log('-----------------------------------------------------------------------------------------');
     console.log('POST "text=Hello Google Home" to:');
     console.log('    http://localhost:' + serverPort + '/google-home-notifier');
     console.log('example:');
     console.log('curl -X POST -d "text=Hello Google Home" http://localhost:' + serverPort + '/google-home-notifier');
+    console.log('-----------------------------------------------------------------------------------------');
+    console.log('\n');
 });
